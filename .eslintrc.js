d559f2cc25824df6f5c3d4339ccb32b101f5ec8d
JiinSeok <seokjiin1073@gmail.com>
2024-06-09 17:36:39 +0900

    [#4] eslintrc에 airbnb 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,21 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'airbnb',
+    'airbnb/hooks',
+    'prettier',
+  ],
   overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
   plugins: ['react'],
-  rules: {},
+  rules: {
+    semi: ['error', 'never'],
+  },
 }
