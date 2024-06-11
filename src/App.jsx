1a4d3fb11852b732ab701ab4d75281ec5ad59519
dyk0224 <88474141+dyk0224@users.noreply.github.com>
2024-06-11 21:34:47 +0900

    [#39] React Route 설정 (#42)

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,23 @@
 import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import AnswerPage from './pages/AnswerPage/AnswerPage'
+import IndividualFeedPage from './pages/IndividualFeedPage/IndividualFeedPage'
+import MainPage from './pages/MainPage/MainPage'
+import QuestionListPage from './pages/QuestionListPage/QuestionListPage'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>src/App.js</code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<MainPage />} />
+        <Route path="list" element={<QuestionListPage />} />
+        <Route path="post">
+          <Route path=":feedId" element={<IndividualFeedPage />}>
+            <Route path="answer" element={<AnswerPage />} />
+          </Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
