6066123a37f12cb364da0748f579852b2c8ff4e1
KWsony <blacksmuff3@gmail.com>
2024-06-13 01:04:32 +0900

    [#26] 카카오, 페이스북 아이콘 공유 기능 구현

--- src/components/SocialShare.jsx
+++ src/components/SocialShare.jsx
@@ -1,10 +1,12 @@
 import iconLink from '../assets/icons/ic_link.svg'
 import iconKakao from '../assets/icons/ic_kakao.svg'
 import iconFacebook from '../assets/icons/ic_facebook.svg'
+import styles from './SocialShare.module.css'
+import Toast from './Toast'
 
 function SocialShare({ onCopyLink, onShareKakao, onShareFacebook, showToast }) {
   return (
-    <div>
+    <div className={styles['icon-container']}>
       <button type="button" onClick={onCopyLink}>
         <img src={iconLink} alt="링크 아이콘" />
       </button>
@@ -14,7 +16,7 @@ function SocialShare({ onCopyLink, onShareKakao, onShareFacebook, showToast }) {
       <button type="button" onClick={onShareFacebook}>
         <img src={iconFacebook} alt="페이스북 아이콘" />
       </button>
-      {showToast && <div>UPL이 복사되었습니다</div>}
+      {showToast && <Toast>URL이 복사되었습니다</Toast>}
     </div>
   )
 }
