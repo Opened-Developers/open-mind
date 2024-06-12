3222b77771c8059f0a0d3e8fafd5a34ddaa6befb
JiinSeok <seokjiin1073@gmail.com>
2024-06-12 16:34:40 +0900

    [#39] 페이지 파일명 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import AnswerPage from './pages/AnswerPage/AnswerPage'
-import IndividualFeedPage from './pages/IndividualFeedPage/IndividualFeedPage'
 import MainPage from './pages/MainPage/MainPage'
-import QuestionListPage from './pages/QuestionListPage/QuestionListPage'
+import FeedListPage from './pages/FeedListPage/FeedListPage'
+import QuestionFeedPage from './pages/QuestionFeedPage/QuestionFeedPage'
+import AnswerFeedPage from './pages/AnswerFeedPage/AnswerFeedPage'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route index element={<MainPage />} />
-        <Route path="list" element={<QuestionListPage />} />
+        <Route path="list" element={<FeedListPage />} />
         <Route path="post">
-          <Route path=":feedId" element={<IndividualFeedPage />}>
-            <Route path="answer" element={<AnswerPage />} />
+          <Route path=":feedId" element={<QuestionFeedPage />}>
+            <Route path="answer" element={<AnswerFeedPage />} />
           </Route>
         </Route>
       </Routes>
