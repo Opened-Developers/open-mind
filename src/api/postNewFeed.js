5242187d6c510b05ff2eb8ec8603c913d10215b5
dyk0224 <88474141+dyk0224@users.noreply.github.com>
2024-06-14 12:53:33 +0900

    [#17] 메인 페이지 개발 (#54)

---
+++ src/api/postNewFeed.js
@@ -0,0 +1,15 @@
+import openmindAxios from './axios'
+
+async function postNewFeed(userName) {
+  try {
+    const response = await openmindAxios.post('/subjects/', {
+      name: userName,
+      team: '7-7',
+    })
+    return response
+  } catch (error) {
+    return error
+  }
+}
+
+export default postNewFeed
