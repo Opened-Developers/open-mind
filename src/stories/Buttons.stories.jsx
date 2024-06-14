07d99f252a96b1c9cc53f4458e4634dcae79cf2a
ylem76 <ylem76@gmail.com>
2024-06-14 10:19:28 +0900

    [#52] 플로팅 버튼 수정

--- src/stories/Buttons.stories.jsx
+++ src/stories/Buttons.stories.jsx
@@ -1,11 +1,15 @@
 import { Children } from 'react'
 import { FillButton, OutlineButton, FloatButton } from '../components/Buttons'
+import { action } from '@storybook/addon-actions'
 
 export default {
   title: 'Buttons',
+  ArgTypes: {
+    onClick: { action: 'clicked' },
+  },
 }
 
-const Template = (args) => <FillButton {...args} />
+const Template = (args) => <FillButton onClick={action('clicked')} {...args} />
 
 export const FillButtons = Template.bind({})
 FillButtons.args = {
@@ -27,12 +31,11 @@ OutlineButtons.args = {
   iconRight: false,
 }
 
-const FloatingTemplate = (args) => <FloatButton {...args} />
+const FloatingTemplate = (args) => (
+  <FloatButton onClick={action('clicked')} {...args} />
+)
 export const FloatingButtons = FloatingTemplate.bind({})
 FloatingButtons.args = {
   children: '질문 작성',
-  onClick: () => {
-    console.log('link')
-  },
   isLink: false,
 }
