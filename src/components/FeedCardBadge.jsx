34a401ee5398347cf47e3e93e341b1cd2ee001b8
KWsony <blacksmuff3@gmail.com>
2024-06-14 23:53:44 +0900

    [#49] 벳지 미답변 질문 답변 완료 표시 버그 수정

--- src/components/FeedCardBadge.jsx
+++ src/components/FeedCardBadge.jsx
@@ -1,6 +1,6 @@
 import styles from './FeedCardBadge.module.css'
 
-export default function FeedCardBadge(isAnswered) {
+export default function FeedCardBadge({ isAnswered }) {
   if (isAnswered) {
     return <div className={styles.badge}>답변 완료</div>
   }
