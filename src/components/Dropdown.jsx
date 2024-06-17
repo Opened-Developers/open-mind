90b1d7edcd274ed53f5e31800dfc921fe490f3b0
ylem76 <ylem76@gmail.com>
2024-06-17 16:18:15 +0900

    [#64] 정렬 드롭다운 키보드 입력 대응

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,20 +1,46 @@
-import React, { cloneElement, useEffect, useState } from 'react'
+import React, { cloneElement, useEffect, useState, useRef } from 'react'
 import styles from './Dropdown.module.css'
 import { ReactComponent as IcArrowUp } from '../assets/icons/ic_arrow_up.svg'
 import { ReactComponent as IcMore } from '../assets/icons/ic_more.svg'
 
-export function SortDropdownItem({ setSelected, setIsExpand, children }) {
+export function SortDropdownItem({
+  setSelected,
+  setIsExpand,
+  children,
+  itemRef,
+}) {
   const handleOnMouseDown = (e) => {
     e.stopPropagation()
+    console.log('item mouse down')
     setIsExpand(false)
     setSelected(children)
   }
 
   const handleKeydown = (e) => {
     e.stopPropagation()
+    e.preventDefault()
+
+    const { activeElement } = document
+    const currentIndex = Array.from(itemRef.current.children).findIndex(
+      (element) => element === activeElement
+    )
+    const currentLength = itemRef.current.children.length
+
     if (e.keyCode === 13) {
       // 엔터 키 눌렀을 때 ul리스트 닫기
       setSelected(children)
+      setIsExpand(false)
+    }
+    if (e.keyCode === 38) {
+      const nextIndex =
+        (((currentIndex - 1) % currentLength) + currentLength) % currentLength
+      itemRef.current.children[nextIndex].focus()
+    }
+    if (e.keyCode === 40) {
+      // ArrowDown key
+      const nextIndex =
+        (((currentIndex + 1) % currentLength) + currentLength) % currentLength
+      itemRef.current.children[nextIndex].focus()
     }
   }
   return (
@@ -27,6 +53,7 @@ export function SortDropdownItem({ setSelected, setIsExpand, children }) {
     </button>
   )
 }
+
 export function MenuDropdownItem({ onClick, setIsExpand, children }) {
   return (
     <button
@@ -52,7 +79,8 @@ const renderChildrenWithProps = (
   children,
   selected,
   setIsExpand,
-  setSelected
+  setSelected,
+  itemRef
 ) =>
   React.Children.map(children, (child) => {
     const childText = child.props.children
@@ -62,6 +90,7 @@ const renderChildrenWithProps = (
       setSelected,
       setIsExpand,
       className: `${isSelected ? styles.selected : ''}`,
+      itemRef,
     })
   })
 
@@ -69,6 +98,7 @@ export function SortDropdown({ children, onChange }) {
   // state
   const [isExpand, setIsExpand] = useState(false)
   const [selected, setSelected] = useState(children[0].props.children)
+  const itemRef = useRef(null)
 
   useEffect(() => {
     onChange(selected)
@@ -76,14 +106,34 @@ export function SortDropdown({ children, onChange }) {
 
   const handleMouseDown = (e) => {
     e.stopPropagation()
+    console.log('handle mouse down')
     setIsExpand((prev) => !prev)
     return false
   }
+
+  const handleKeydown = (e) => {
+    e.stopPropagation()
+    console.log(itemRef.current)
+    if (e.keyCode === 40) {
+      // arrow down
+      e.preventDefault()
+      if (!isExpand) {
+        setIsExpand(true)
+      } else {
+        console.log(itemRef.current.children[0])
+        itemRef.current.children[0].focus()
+      }
+    }
+  }
+
   return (
     <div
       className={`caption-1 ${styles.wrapper} ${isExpand ? styles.expand : ''}`}
-      onBlur={() => {
-        setIsExpand(() => false)
+      onBlur={(event) => {
+        if (!event.currentTarget.contains(event.relatedTarget)) {
+          console.log('blur')
+          setIsExpand(false)
+        }
       }}
     >
       <button
@@ -91,6 +141,7 @@ export function SortDropdown({ children, onChange }) {
         id="selectButton"
         aria-expanded="false"
         onClick={handleMouseDown}
+        onKeyDown={handleKeydown}
         className={`caption-1 ${styles['btn-trigger']}`}
       >
         {selected}
@@ -101,12 +152,14 @@ export function SortDropdown({ children, onChange }) {
           className={styles.dropdown}
           role="listbox"
           aria-labelledby="selectButton"
+          ref={itemRef}
         >
           {renderChildrenWithProps(
             children,
             selected,
             setIsExpand,
-            setSelected
+            setSelected,
+            itemRef
           )}
         </ul>
       )}
