9f82ae771d4f15c58677b0a8975a39776394affb
KWsony <blacksmuff3@gmail.com>
2024-06-17 19:25:20 +0900

    [#58] FeedCard 모듈 css 파일 import

--- src/components/FeedCard.jsx
+++ src/components/FeedCard.jsx
@@ -3,6 +3,7 @@ import FeedCardQuestion from './FeedCardQuestion'
 import FeedCardAnswer from './FeedCardAnswer'
 import FeedCardAnswerEdit from './FeedCardAnswerEdit'
 import FeedCardReaction from './FeedCardReaction'
+import styles from './FeedCard.module.css'
 
 export default function FeedCard({ question, isMyFeed, profile = null }) {
   return (
