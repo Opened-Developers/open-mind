a150186af3ec1a66262716886d14145410a9aa59
ylem76 <ylem76@gmail.com>
2024-06-19 14:19:59 +0900

    [#73] 토스트 추가

--- src/modules/utils.js
+++ src/modules/utils.js
@@ -19,3 +19,14 @@ export const createLocalUserId = (userId) => {
   const newLocalUserId = userId
   localStorage.setItem('userId', newLocalUserId)
 }
+
+/**
+ * 현재 시간과 랜덤 숫자를 이용해 고유한 id를 만드는 함수
+ * @returns {number} id값
+ */
+export const getUniqueId = () => {
+  const timestamp = new Date().getTime()
+  const random = Math.floor(Math.random() * 1000)
+  const uniqueId = `${timestamp}${random}`
+  return uniqueId
+}
