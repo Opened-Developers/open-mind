c7385cf886f4c2ca13ca16e2603e9ab09ff7ad67
JiinSeok <seokjiin1073@gmail.com>
2024-06-20 18:01:29 +0900

    [#62] 답변 컴포넌트 스타일링

--- src/components/FeedCardAnswer.jsx
+++ src/components/FeedCardAnswer.jsx
@@ -1,14 +1,26 @@
 import getRelativeDate from '../utils/getRelativeDate'
+import styles from './FeedCardAnswer.module.css'
 
 export default function FeedCardAnswer({ question, profile }) {
   return (
-    <div>
-      <img src={profile.imageSource} alt={'프로필 이미지'} />
+    <div className={styles['answer-layout']}>
+      <img
+        className={styles['user-img']}
+        src={profile.imageSource}
+        alt={'프로필 이미지'}
+      />
       <div>
-        <p>{profile.name}</p>
-        <p>{getRelativeDate(question.answer.createdAt)}</p>
-        <p>{question.answer.content}</p>
-        {question.answer.isRejected && <p>답변 거절</p>}
+        <div className={styles['answer-info']}>
+          <p className={styles['user-name']}>{profile.name}</p>
+          <p className={styles['answer-date']}>
+            {getRelativeDate(question.answer.createdAt)}
+          </p>
+        </div>
+        {question.answer.isRejected ? (
+          <p className={styles['answer-rejecting']}>답변 거절</p>
+        ) : (
+          <p className={styles['answer-content']}>{question.answer.content}</p>
+        )}
       </div>
     </div>
   )
