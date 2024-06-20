a201b1e483ea1d2ecd97a61c72165374228fc962
ylem76 <ylem76@gmail.com>
2024-06-20 17:42:20 +0900

    [#85] 404 페이지 추가

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import MainPage from './pages/MainPage/MainPage'
 import QuestionListPage from './pages/QuestionListPage/QuestionListPage'
 import IndividualFeedPage from './pages/IndividualFeedPage/IndividualFeedPage'
+import NotFoundPage from './pages/NotFound/NotFoundPage'
 import AnswerPageContainer from './pages/AnswerPage/AnswerPageContainer'
 import { ToastContextProvider } from './contexts/toastContextProvider'
 import MainLayout from './components/layouts/MainLayout'
@@ -18,6 +19,7 @@ function App() {
               <Route path=":feedId" element={<IndividualFeedPage />} />
               <Route path=":feedId/answer" element={<AnswerPageContainer />} />
             </Route>
+            <Route path="*" element={<NotFoundPage />} />
           </Route>
         </Routes>
       </BrowserRouter>
