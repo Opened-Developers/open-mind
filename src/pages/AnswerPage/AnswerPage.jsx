05ea799a682c67ee99c31af223ca16b3f9fced31
JiinSeok <seokjiin1073@gmail.com>
2024-06-21 11:56:58 +0900

    [#62] 답변하기 페이지 질문 안 뜨는 문제 수정, 무한 스크롤 추가

--- src/pages/AnswerPage/AnswerPage.jsx
+++ src/pages/AnswerPage/AnswerPage.jsx
@@ -1,3 +1,5 @@
+import { useCallback, useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 import styles from './AnswerPage.module.css'
 import SocialShareContainer from '../../components/SocialShareContainer'
 import FeedCardList from '../../components/FeedCardList'
@@ -5,7 +7,69 @@ import { FloatButton } from '../../components/Buttons'
 import logo from '../../assets/images/img_logo.png'
 import openMindImg from '../../assets/images/img_openmind.png'
 
-function AnswerPage({ feedId, profile, errorMessage = null, onClick }) {
+import getFeedQuestions from '../../api/getFeedQuestions'
+
+import Toast from '../../components/Toast'
+
+function AnswerPage({ profile, errorMessage = null, onClick }) {
+  const { feedId } = useParams()
+  const [questions, setQuestions] = useState([])
+  const [questionCount, setQuestionCount] = useState(0)
+  const [offset, setOffset] = useState(0)
+  const [next, setNext] = useState(null)
+  const [errorInfo, setErrorInfo] = useState(null)
+  const [isLoading, setIsLoading] = useState(false)
+  const LIMIT = 10
+  const listEnd = document.querySelector('.list-end')
+
+  const handleLoadQuestions = useCallback(async () => {
+    if (isLoading) {
+      return
+    }
+    let response
+    try {
+      setIsLoading(true)
+      response = await getFeedQuestions({ feedId, offset, limit: LIMIT })
+      if (offset === 0) {
+        setQuestions(response.results)
+      } else {
+        setQuestions((prevQuestions) => [...prevQuestions, ...response.results])
+      }
+      setQuestionCount(response.count)
+      setOffset((prevOffset) => prevOffset + response.results.length)
+      setNext(response.next)
+      setErrorInfo(null)
+    } catch (error) {
+      setErrorInfo(error.message)
+    } finally {
+      setIsLoading(false)
+    }
+  }, [feedId, offset, isLoading])
+
+  useEffect(() => {
+    if (offset === 0) {
+      handleLoadQuestions().then()
+    }
+  }, [feedId, offset, handleLoadQuestions])
+
+  const observer = new IntersectionObserver(
+    (entries) => {
+      if (entries[0].isIntersecting) {
+        observer.unobserve(listEnd)
+        handleLoadQuestions().then()
+      }
+    },
+    { threshold: 0.5 }
+  )
+
+  if (listEnd) {
+    observer.observe(listEnd)
+  }
+
+  if (errorInfo) {
+    return <Toast>{errorInfo}</Toast>
+  }
+
   return (
     <div className={styles.body}>
       <header className={styles.header}>
@@ -32,10 +96,16 @@ function AnswerPage({ feedId, profile, errorMessage = null, onClick }) {
             feedId={feedId}
             isMyFeed
             profile={profile}
-            questions={[]}
-            questionCount={0}
+            questions={questions}
+            questionCount={questionCount}
           />
         </section>
+        <div
+          className="list-end"
+          style={next === null ? { display: 'none' } : {}}
+        >
+          로딩 중 ...
+        </div>
         {errorMessage?.message && (
           <div className={styles.error}>{errorMessage.message}</div>
         )}
