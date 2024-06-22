e1bba238a720cff703511ce5431121fd374d4eac
JiinSeok <seokjiin1073@gmail.com>
2024-06-22 20:15:14 +0900

    [#119] 공통 함수 loadProfile App.jsx로 이동

--- src/pages/IndividualFeedPage/IndividualFeedPage.jsx
+++ src/pages/IndividualFeedPage/IndividualFeedPage.jsx
@@ -1,38 +1,27 @@
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import { Link, useParams } from 'react-router-dom'
 import FeedCardList from '../../components/FeedCardList'
 import SocialShareContainer from '../../components/SocialShareContainer'
 import { FloatButton } from '../../components/Buttons'
 import QuestionModal from '../../components/QuestionModal/QuestionModal'
-import getProfileById from '../../api/getProfileById'
 import Toast from '../../components/Toast'
 import { getLocalUserId } from '../../modules/utils'
 import styles from './IndividualFeedPage.module.css'
 import openMindImg from '../../assets/images/img_openmind.png'
 import logo from '../../assets/images/img_logo.png'
 
-export default function IndividualFeedPage() {
+export default function IndividualFeedPage({
+  loadProfile,
+  profile,
+  errorMessage,
+}) {
   const { feedId } = useParams()
-  const [profile, setProfile] = useState(null)
-  const [errorInfo, setErrorInfo] = useState(null)
 
   const localUserId = getLocalUserId()
 
-  const handleLoadProfile = async (id) => {
-    let response
-    try {
-      response = await getProfileById(id)
-      setProfile(response)
-      setErrorInfo(null)
-    } catch (error) {
-      setErrorInfo(error.message)
-    }
-    return null
-  }
-
   useEffect(() => {
-    handleLoadProfile(feedId).then()
-  }, [feedId])
+    loadProfile(feedId).then()
+  }, [feedId, loadProfile])
 
   if (profile) {
     return (
@@ -66,7 +55,7 @@ export default function IndividualFeedPage() {
             <QuestionModal profile={profile} />
           )}
         </div>
-        {errorInfo && <Toast>{errorInfo}</Toast>}
+        {errorMessage && <Toast>{errorMessage}</Toast>}
       </div>
     )
   }
