250f524f468acd76b504ba3f5414984e4a52b60c
JiinSeok <seokjiin1073@gmail.com>
2024-06-23 18:20:33 +0900

    [#125] 파라미터 바뀌면 질문 새로 로드

--- src/pages/AnswerPage/AnswerPageContainer.jsx
+++ src/pages/AnswerPage/AnswerPageContainer.jsx
@@ -41,7 +41,10 @@ function AnswerPageContainer({
     if (offset === 0) {
       onLoadMore(feedId).then()
     }
-  }, [feedId, offset, onLoadMore])
+    if (feedId !== questions[0]?.feedId) {
+      onLoadNew(feedId)
+    }
+  }, [feedId, offset, onLoadMore, onLoadNew, questions])
 
   if (profile) {
     return (
